4463b15768519f7c9a7ad82b3d8d1649d7f29c8c
nayabayesha179 <165317610+nayabayesha179@users.noreply.github.com>
2024-04-23 17:47:34 +0500

    Update homePage.blade.php

--- resources/views/layouts/homePage.blade.php
+++ resources/views/layouts/homePage.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>AdminLTE 3 | Dashboard</title>
+    <title>AboveMax</title>
     @include('panels.style')
 </head>
 <body class="hold-transition sidebar-mini layout-fixed">
